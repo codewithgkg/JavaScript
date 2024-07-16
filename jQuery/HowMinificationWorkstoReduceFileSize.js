a90a77a507d05a4aa92e36bedea061dd25a4a0eb
codewithgkg <gauravgautam891@gmail.com>
2024-07-16 09:24:44 +0530

    Add files via upload

---
+++ jQuery/HowMinificationWorkstoReduceFileSize.js
@@ -0,0 +1,20 @@
+/* margin:0;
+border:0;
+padding:1px;
+width:1px;
+zoom:1",
+
+l.inlineBlockNeedsLayout=a=3===b.
+offsetWidth,a&&(c.style.zoom=1)),
+
+c.removeChild(e))}),
+
+function(){var a=d.createElement("div");
+
+l.deleteExpando=!0;try{delete a.test}catch(b)
+{l.deleteExpando=!1}a=null}();var M=function(a){var b=n.noData[(a.nodeName+" ") */
+
+/* We can use minify to get the minified version of Javascript or CSs */
+/* https://www.minifier.org/ */
+
+/* for example : https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js */
